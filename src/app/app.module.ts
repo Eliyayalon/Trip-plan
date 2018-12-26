80bea2f6854d6c9e50517a080db5155ae7e0e0ad
Eliya <eliyayalon@gmail.com>
2018-12-26 19:50:51 +0200

    #3 timeline-done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,23 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-
 import { AlertModule } from 'ngx-bootstrap/alert';
 import { BsDatepickerModule, BsDropdownModule, ModalModule, PopoverModule } from 'ngx-bootstrap';
-
 import { VistimelineComponent } from './vistimeline/vistimeline.component';
 
 
 @NgModule({
   declarations: [
-    
-    AppComponent,
-    
-   
-    VistimelineComponent,
+  AppComponent,
+  VistimelineComponent,
     
   ],
   imports: [
