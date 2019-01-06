246f1c9f000a030559569e720425bca97abf7b49
Eliya <eliyayalon@gmail.com>
2019-01-06 13:03:50 +0200

    change name of utem

--- src/app/vistimeline/vistimeline.component.ts
+++ src/app/vistimeline/vistimeline.component.ts
@@ -17,6 +17,7 @@ export class VistimelineComponent implements OnInit {
 
   constructor() { 
     this.getTimelineData();
+    
   }
 
   ngOnInit() {
@@ -26,29 +27,40 @@ export class VistimelineComponent implements OnInit {
   ngAfterViewInit() {     
     this.tlContainer = this.timelineContainer.nativeElement;       
     this.timeline = new vis.Timeline(this.tlContainer, this.data, this.options);  
-    
-    
   }
+  
 
 getTimelineData() {
       // Create a DataSet (allows two way data-binding)
       this.data = new vis.DataSet([
-         {id: 1, content: 'item1', start: '2018-12-29' },
+         {id: 1, content: 'item1', start: '2018-12-29', end:'2018-12-30' },
        
         
       ]);
       
+      
         this.options = {
+          
         editable: true,// add/remove item
         showTooltips: true,
         clickToUse: true,
+        
+        onUpdate: function (item, callback) { //change name of item
+          item.content = prompt('Edit items text:', item.content);
+          if (item.content != null) {
+            callback(item); 
+          }
+          else {
+            callback(null); 
+          }
+        },
         tooltip: {
           followMouse: true,
           overflowMethod: 'cap'
         },
         margin: {
           item: 20,
-          axis: 40
+          axis: 20
         }
       };
       
